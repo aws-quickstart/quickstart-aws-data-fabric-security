e6afba07d65cc80db9526540cf4492209d1493cb
Jenifer Wang <djenifwa@amazon.com>
2023-04-14 10:25:42 -0700

    Removed resource files

--- resources/immuta/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if kubectl get namespace/$NAMESPACE
-then
-    echo "### Uninstalling Immuta ###"
-    kubectl delete namespace $NAMESPACE
-    echo "### Immuta uninstall complete ###"
-else
-    echo "### Namespace not found. Nothing to do ###"
-fi
--- resources/radiantlogic/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if kubectl get namespace/$NAMESPACE
-then
-    echo "### Uninstalling Radiant Logic ###"
-    kubectl delete namespace $NAMESPACE
-    echo "### Radiant Logic uninstall complete ###"
-else
-    echo "### Namespace not found. Nothing to do ###"
-fi
